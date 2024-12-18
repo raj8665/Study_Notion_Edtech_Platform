625498cab96e2dde3f4cf732b1e87a4151fb6a12
Raj Chaturvedi <rajchaturvedi07032004@gmail.com>
2024-12-18 16:03:55 +0530

    SignUp and LoginForm Created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,35 @@ import "./App.css";
 import {Route,Routes} from "react-router-dom";
 import Home from "./pages/Home"
 import Navbar from "./components/common/Navbar"
-
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ForgotPassword from "./pages/ForgotPassword";
+import OpenRoute from "./components/core/Auth/OpenRoute";
 function App() {
   return (
    <div className=" w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
           <Navbar />
           <Routes>
               <Route path="/" element={<Home/>} />
+              <Route
+          path="/login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
+
+        <Route
+          path="/signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
+        
+        <Route path="/forgot-password" element={<ForgotPassword />} />
           </Routes>
    </div>
   );
