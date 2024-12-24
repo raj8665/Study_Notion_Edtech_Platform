9f132f272937e8050127e1dad6b70b57dafd7cc4
Raj Chaturvedi <rajchaturvedi07032004@gmail.com>
2024-12-24 17:37:03 +0530

    Admin Pannel Created and some ui fixed ....

--- src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
-
+import React from "react"
 import IconBtn from "../../../common/IconBtn"
 import { buyCourse } from "../../../../services/operations/studentFeaturesAPI"
 
@@ -12,18 +12,25 @@ export default function RenderTotalAmount() {
   const dispatch = useDispatch()
 
   const handleBuyCourse = () => {
-    const courses = cart.map((course) => course._id)
-    buyCourse(token, courses, user, navigate, dispatch)
+    const courses = cart.map((course) => course._id);
+    if (token) {
+      const courses = cart.map((course) => course._id);
+      buyCourse(token, courses, user, navigate, dispatch)
+    }
+    else {
+      navigate('/login');
+    }
+
   }
 
   return (
-    <div className="min-w-[280px] rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6">
+    <div className="min-w-[280px] rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-3 md:p-6">
       <p className="mb-1 text-sm font-medium text-richblack-300">Total:</p>
-      <p className="mb-6 text-3xl font-medium text-yellow-100">₹ {total}</p>
+      <p className="mb-6 text-3xl font-medium text-yellow-100 crimson">₹ {total}</p>
       <IconBtn
         text="Buy Now"
         onclick={handleBuyCourse}
-        customClasses="w-full justify-center"
+        customClasses="flex items-center bg-yellow-50 cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-richblack-900 w-full justify-center"
       />
     </div>
   )
